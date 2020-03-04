3d22791ddeba1620d145378d457383bf576c6d35
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-03-04 08:48:50 -0600

    Show a number on the scoreboard when given one

--- lib/components/number.ex
+++ lib/components/number.ex
@@ -9,12 +9,25 @@ defmodule Fenway.Component.Number do
   def info(_), do: "error"
   def verify(data), do: {:ok, data}
 
-  def init({name, _number}, _opts) do
+  def init({name, number}, _opts) do
     {:ok, _} = Registry.register(Registry.Components, name, [])
 
-    graph = Graph.build([])
-    |> rectangle({30, 40}, stroke: {1, @border_color})
-
-    {:ok, %{}, push: graph}
+    {:ok, %{}, push: graph_for(number)}
   end
+
+  def handle_cast({:number, number}, state) do
+    {:noreply, state, [push: graph_for(number)]}
+  end
+
+  defp graph_for(:blank) do
+    Graph.build([])
+    |> rectangle({30, 40}, stroke: {1, @border_color})
+  end
+  defp graph_for(number) when number >= 0 and number <= 99 do
+    graph_for(:blank)
+    |> text("#{number}", opts_for(number))
+  end
+
+  defp opts_for(n) when n >= 0 and n <= 9, do: [fill: :white, font_size: 40, translate: {5, 32}]
+  defp opts_for(n) when n >= 10 and n <= 99, do: [fill: :white, font_size: 32, translate: {0, 32}]
 end
