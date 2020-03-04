05edcc5a0b602b973559f5b647af49b02b4e86d3
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-03-04 08:33:25 -0600

    Register each number box with a name

--- lib/components/number.ex
+++ lib/components/number.ex
@@ -9,7 +9,9 @@ defmodule Fenway.Component.Number do
   def info(_), do: "error"
   def verify(data), do: {:ok, data}
 
-  def init(_number, _opts) do
+  def init({name, _number}, _opts) do
+    {:ok, _} = Registry.register(Registry.Components, name, [])
+
     graph = Graph.build([])
     |> rectangle({30, 40}, stroke: {1, @border_color})
 
