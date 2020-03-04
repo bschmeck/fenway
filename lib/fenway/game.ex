a0d893484d2430606967e761b32ecea4b241813b
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2020-03-04 13:48:27 -0600

    Refactor how data is retrieved from the game feed JSON

--- lib/fenway/game.ex
+++ lib/fenway/game.ex
@@ -8,20 +8,20 @@ defmodule Fenway.Game do
   end
 
   def parse(json) do
-    %__MODULE__{at_bat: batter_number(json),
-                balls: balls(json),
-                strikes: strikes(json),
-                outs: outs(json),
-                home_hits: home_hits(json),
-                home_runs: home_runs(json),
-                home_errors: home_errors(json),
-                away_hits: away_hits(json),
-                away_runs: away_runs(json),
-                away_errors: away_errors(json)
+    %__MODULE__{at_bat: retrieve(json, :batter_number),
+                balls: retrieve(json, :balls),
+                strikes: retrieve(json, :strikes),
+                outs: retrieve(json, :outs),
+                home_hits: retrieve(json, :home_hits),
+                home_runs: retrieve(json, :home_runs),
+                home_errors: retrieve(json, :home_errors),
+                away_hits: retrieve(json, :away_hits),
+                away_runs: retrieve(json, :away_runs),
+                away_errors: retrieve(json, :away_errors)
     }
   end
 
-  defp batter_number(json) do
+  defp retrieve(json, :batter_number) do
     batter_id = dig(json, ~w[liveData linescore offense batter id])
 
     raw = dig(json, ["gameData", "players", "ID#{batter_id}", "primaryNumber"])
@@ -32,41 +32,26 @@ defmodule Fenway.Game do
     end
   end
 
-  defp balls(json) do
-    dig(json, ~w[liveData plays currentPlay count balls])
-  end
+  defp retrieve(json, path) when is_list(path), do: dig(json, path)
+  defp retrieve(json, :balls), do: retrieve(json, path_to(:balls))
+  defp retrieve(json, :outs), do: retrieve(json, path_to(:outs))
+  defp retrieve(json, :strikes), do: retrieve(json, path_to(:strikes))
+  defp retrieve(json, :away_errors), do: retrieve(json, path_to(:away_errors))
+  defp retrieve(json, :away_hits), do: retrieve(json, path_to(:away_hits))
+  defp retrieve(json, :away_runs), do: retrieve(json, path_to(:away_runs))
+  defp retrieve(json, :home_errors), do: retrieve(json, path_to(:home_errors))
+  defp retrieve(json, :home_hits), do: retrieve(json, path_to(:home_hits))
+  defp retrieve(json, :home_runs), do: retrieve(json, path_to(:home_runs))
 
-  defp outs(json) do
-    dig(json, ~w[liveData plays currentPlay count outs])
-  end
-
-  defp strikes(json) do
-    dig(json, ~w[liveData plays currentPlay count strikes])
-  end
-
-  defp away_errors(json) do
-    dig(json, ~w[liveData boxscore teams away teamStats fielding errors])
-  end
-
-  defp home_errors(json) do
-    dig(json, ~w[liveData boxscore teams home teamStats fielding errors])
-  end
-
-  defp away_runs(json) do
-    dig(json, ~w[liveData boxscore teams away teamStats batting runs])
-  end
-
-  defp home_runs(json) do
-    dig(json, ~w[liveData boxscore teams home teamStats batting runs])
-  end
-
-  defp away_hits(json) do
-    dig(json, ~w[liveData boxscore teams away teamStats batting hits])
-  end
-
-  defp home_hits(json) do
-    dig(json, ~w[liveData boxscore teams home teamStats batting hits])
-  end
+  defp path_to(:balls), do: ~w[liveData plays currentPlay count balls]
+  defp path_to(:outs), do: ~w[liveData plays currentPlay count outs]
+  defp path_to(:strikes), do: ~w[liveData plays currentPlay count strikes]
+  defp path_to(:away_errors), do: ~w[liveData boxscore teams away teamStats fielding errors]
+  defp path_to(:away_hits), do: ~w[liveData boxscore teams away teamStats batting hits]
+  defp path_to(:away_runs), do: ~w[liveData boxscore teams away teamStats batting runs]
+  defp path_to(:home_errors), do: ~w[liveData boxscore teams home teamStats fielding errors]
+  defp path_to(:home_hits), do: ~w[liveData boxscore teams home teamStats batting hits]
+  defp path_to(:home_runs), do: ~w[liveData boxscore teams home teamStats batting runs]
 
   defp dig(value, []), do: value
   defp dig(map, [key | rest]), do: map |> Map.get(key) |> dig(rest)
